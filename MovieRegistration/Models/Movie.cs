831c13389995d876eb1321c1f7e2a5fb05149c96
Wendi Magee <wendimagee@gmail.com>
2021-02-17 19:50:02 -0500

    Add project files.

---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public enum Category
+    {
+        Comedy,
+        Drama,
+        Horror,
+        Romance,
+        Action,
+        Indie,
+        Adventure,
+        Propaganda,
+        Animation
+    }
+    public class Movie
+    {
+
+ 
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public Category Genre { get; set; }
+        public DateTime Year { get; set; }
+        public List<string> Actors { get; set; }
+        public List<string> Directors { get; set; }
+    }
+}
